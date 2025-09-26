9bcef94e7580b8b95284544d10bf5dfff4fa7776
Christopher <135161034+Christopher-97@users.noreply.github.com>
2025-09-26 09:53:37 -0300

    Delete Proyectosemestral/Proyectosemestral directory

--- Proyectosemestral/Proyectosemestral/src/app/pages/reportes/reportes.page.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-reportes',
-  templateUrl: './reportes.page.html',
-  styleUrls: ['./reportes.page.scss'],
-  standalone: false,
-})
-export class ReportesPage {}
